00f4ea3bbb48a7668df67f9661af6d0179861e51
Hector Alberto Diaz Gomez <146489883+hectordiazgomez@users.noreply.github.com>
2024-09-16 14:21:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const port = 3000;
+const port = 5000;
 
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to the API" });
