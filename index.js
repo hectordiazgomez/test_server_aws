18d7ef731f03d372b25e0aa5820a2ecc229a1b33
Hector Alberto Diaz Gomez <146489883+hectordiazgomez@users.noreply.github.com>
2024-12-31 11:58:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ let data = [
 ];
 
 app.get("/", (req, res) => {
-    res.json({ message: "La mitad de mi alma se ha quedado contigo x3" });
+    res.json({ message: "De tanto amarnos nos volvimos dos extraños" });
 });
 
 app.get("/hello", (req, res) => {
