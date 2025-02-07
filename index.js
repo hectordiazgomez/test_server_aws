046d5eabd779b9752b4a24054eb386038785a985
Hector Alberto Diaz Gomez <146489883+hectordiazgomez@users.noreply.github.com>
2025-02-06 19:09:23 -0800

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ let data = [
 ];
 
 app.get("/", (req, res) => {
-    res.json({ message: "Testing splits of CPU and RAM 2" });
+    res.json({ message: "Testing splits of CPU and RAM 3" });
 });
 
 app.get("/hello", (req, res) => {
