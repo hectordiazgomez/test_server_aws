919f13f681e80153cc9f4b2afb46af4249217387
Hector Alberto Diaz Gomez <146489883+hectordiazgomez@users.noreply.github.com>
2025-02-07 15:53:55 -0800

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 const port = 8000;
 
 let data = [
-    { id: 1, name: "Item 1", value: 100 },
+    { id: 1, name: "Item 1", value: 600 },
     { id: 2, name: "Item 2", value: 200 },
     { id: 3, name: "Item 3", value: 300 },
 ];
