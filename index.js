0b8eda10a52f64e200bae3d9ae199d9ba669f7d9
Hector Alberto Diaz Gomez <146489883+hectordiazgomez@users.noreply.github.com>
2025-02-10 22:37:38 -0800

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ let data = [
 ];
 
 app.get("/", (req, res) => {
-    res.json({ message: "Test seis de funcionamiento" });
+    res.json({ message: "Test siete de funcionamiento" });
 });
 
 app.get("/hello", (req, res) => {
